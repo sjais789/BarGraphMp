a99f9c17f75c719d35100bfa221a4639ddfaf1cc
Shubham <jais.shubham26@gmail.com>
2017-04-03 07:03:22 +0530

    Initial commit

---
+++ app/src/main/java/app/num/bargraphmp/MainActivity.java
@@ -0,0 +1,53 @@
+package app.num.bargraphmp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BarChart barChart = (BarChart) findViewById(R.id.chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("Java");
+        labels.add("Javascript");
+        labels.add("Blockly");
+        labels.add("GoogleAppEngine");
+        labels.add("Closure");
+        labels.add("JWT");
+
+
+
+        BarData data = new BarData(labels, dataset);
+        barChart.setData(data);
+        barChart.animateY(5000);
+
+    }
+}
